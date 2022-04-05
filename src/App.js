3224fbb7c83975efed084838bb215d9831cac1fd
Juninho <joaofbjunior01@gmail.com>
2022-04-05 19:34:14 -0300

    uploading webpage structure to develop githubpages

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+
+import './app.css'
+import logo from './logo.png'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        
+        <header className="App-header">  
+        <h3 class="texto"><a href="http://www.consumistas.com.br">consumistas.com.br</a></h3>
+          <h1 class="texto">
+            Site em desenvolvimento :)
+          </h1>
+
+
+        </header>
+        <h3>Deseja contribuir? chama no telegram: @freitasdevjr</h3>
+      </div>
+    )
+  }
+}
+
+export default App
